7f6b80639f2a6d401ece46dc11fa3af58884acfa
Angela Ruecker <angelrck@gmail.com>
2025-10-18 20:41:40 +0200

    Envoi du script de login et gestion des erreurs

--- login.js
+++ login.js
@@ -1,33 +1,58 @@
+// ==========================
 // Séparer le code:
-// → script.js = front public
-// → login.js = authentification et gestion des accès
+// script.js = front public
+// login.js = authentification et gestion des accès
+// ==========================
 
 export function ajoutListenerLogin() {
     const form = document.getElementById('login-form');
 
-form.addEventListener("submit", function(event) {
-  event.preventDefault();
-  console.log("formulaire soumis !");
-
-// Création de l'objet envoyé par le formulaire
-const formLogin = {
-    email : event.target.querySelector("[name=email]").value,
-    password : event.target.querySelector("[name=password]").value
-};
-
-// Création de la charge utile json
-const chargeUtile = JSON.stringify(formLogin);
-
-// Appel de la fonction fetch 
-fetch("http://localhost:5678/api/users/login", {
-method: "POST",
-headers: { "Content-Type": "application/json" },
-body: chargeUtile,
+    form.addEventListener("input", () => {
+    document.getElementById("login-error").innerHTML = "";
     });
-});
+
+    form.addEventListener("submit", async function(event) {
+        event.preventDefault();
+        console.log("formulaire soumis !");
+
+    // ==========================
+    // Création de l'objet envoyé par le formulaire
+    // ==========================
+
+    const formLogin = {
+        email : event.target.querySelector("[name=email]").value,
+        password : event.target.querySelector("[name=password]").value
+    };
+
+    // ==========================
+    // Création de la charge utile json
+    // ==========================
+
+    const chargeUtile = JSON.stringify(formLogin);
+
+    // ==========================
+    // Appel de la fonction fetch 
+    // ==========================
+
+    const response = await fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: chargeUtile,
+    });
+
+    const data = await response.json()
+
+    if (response.status === 200) {
+    localStorage.setItem("token", data.token);
+    } else {
+    document.getElementById("login-error").innerHTML = "Une erreur est survenue, veuillez réessayer.";
+    }
+    });
 }
 
+// ==========================
 // On appelle la fonction:
-ajoutListenerLogin();
+// ==========================
 
+ajoutListenerLogin();
 
--- script.js
+++ script.js
@@ -1,37 +1,43 @@
 // ==========================
 // Récupérer les works depuis l'API :
 // ==========================
-let works = [];
-
-// ==========================
 // (Await a besoin d'une fonction async)
 // GET est le verbe utilisé par défaut sur la fonction fetch
 // ==========================
+
+let works = [];
 
 async function fetchWorks() {
   const response = await fetch("http://localhost:5678/api/works");
   works = await response.json();
   loadWorks(); // affiche tout au démarrage
 }
-
+// ==========================
+// On appelle la fonction
+// ==========================
 fetchWorks();
 
 
 // ==========================
-// Fonction d'affichage des works (avec filtre optionnel). On met "0" par défaut
+// Fonction d'affichage des works (avec filtre). On met "0" par défaut
 // ==========================
+
 function loadWorks(filterId = "0") {
   const gallery = document.querySelector(".gallery");
   gallery.innerHTML = ""; // vider la galerie avant d'ajouter les nouvelles images
 
   let worksToDisplay;
   if (filterId == "0") {
-    worksToDisplay = works; // toutes les œuvres
+    worksToDisplay = works; // toutes les works
   } else {
-    worksToDisplay = works.filter(work => work.categoryId == filterId); // filtre
+    worksToDisplay = works.filter(work => work.categoryId == filterId); // affiche les works du 
+    // filtre choisi
   }
 
-  // Créer les figures dynamiquement
+// ==========================
+  // Créer les figures et les attacher à la gallerie
+  // ==========================
+
   for (let i = 0; i < worksToDisplay.length; i++) {
     const work = worksToDisplay[i];
     const figure = document.createElement("figure");
@@ -47,10 +53,10 @@ function loadWorks(filterId = "0") {
   }
 }
 
-
 // ==========================
 // Gestion des boutons de filtres
 // ==========================
+
 const buttons = document.querySelectorAll(".filters button");
 
 buttons.forEach(button => {
